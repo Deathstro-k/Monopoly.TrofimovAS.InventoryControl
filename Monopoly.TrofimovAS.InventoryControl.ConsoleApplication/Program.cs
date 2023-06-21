4111b6387691b0a31183ec5371dce50147008069
Trofimov Alexandr <sanechichek.trofimov@gmail.com>
2023-06-21 18:52:03 +0300

    Добавьте файлы проекта.

---
+++ Monopoly.TrofimovAS.InventoryControl.ConsoleApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Monopoly.TrofimovAS.InventoryControl.ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
